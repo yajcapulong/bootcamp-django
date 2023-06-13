b205271ea00adc401145f1cb44691e9c2b08f211
okeneru <yaj.cplng@gmail.com>
2023-06-13 21:57:36 +0800

    Resolved sorting of tweets

--- tweets/models.py
+++ tweets/models.py
@@ -1,6 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.conf import settings
+from django.urls import reverse
 
 
 def validation_content(value):  # Content Validation
@@ -16,5 +17,11 @@ class Tweet (models.Model):
     updated = models.DateTimeField(auto_now=True)
     createdAt = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-createdAt']
+
     def __str__(self):
         return str(self.content)
+
+    def get_absolute_url(self):
+        return reverse("tweet:detail_view", kwargs={"pk":self.pk})
